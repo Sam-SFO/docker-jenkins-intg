a84448a013dee070f005114368192aa7aa4e227d
Sanjay Bahal <sb6325@yahoo.com>
2020-09-22 12:47:14 -0500

    first commit

---
+++ src/main/java/com/sbahal/docker_jenkins_intg/App.java
@@ -0,0 +1,13 @@
+package com.sbahal.docker_jenkins_intg;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
